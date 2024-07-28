ce78a8a99de216b036b4800cf660167c2e796447
Thang Huynh <lthang221198@gmail.com>
2024-07-28 18:05:31 +0700

    fix ticket1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String test2 = "TEst 2";
+        String ticket1 = "This is the fix for ticket 1";
         System.out.println("Hello world!");
     }
 }
