19c4100b2547328b4e832652ec339f2ad7d82123
Thang Huynh <lthang221198@gmail.com>
2024-07-28 17:47:44 +0700

    commit 1 testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int result;
+        result = a/2 + b;
         System.out.println("Hello world!");
     }
 }
