2cc9881eebd2263c1ab956fd3150e194a252c9e0
Thang Huynh <lthang221198@gmail.com>
2024-07-28 17:59:03 +0700

    Test git reset --soft

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String test = "Test again";
+        String test2 = "TEst 2";
         System.out.println("Hello world!");
     }
 }
